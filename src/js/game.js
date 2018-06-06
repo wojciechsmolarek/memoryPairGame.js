ac951d8c8d42f90eb02d6265cf8d29e992d4d8a1
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2018-06-06 10:58:49 +0200

    created html, css, js files

---
+++ src/js/game.js
@@ -0,0 +1,11 @@
+(function() {
+
+let container = document.querySelector(".wrapper");
+
+for (let i = 0; i < 18; i++) {
+  let divider = document.createElement("div");
+  // divider.style.background = randColor;
+  container.appendChild(divider);
+}
+
+})();
